6e1e7e7ba3bbf3f74b9eba8e703cfd87bfadda0b
Ben Weinstein-Raun <root@benwr.net>
2020-01-18 16:26:54 -0800

    some attempted performance improvements

--- src/GraphSearch/AStar.hs
+++ src/GraphSearch/AStar.hs
@@ -1,45 +1,9 @@
 module GraphSearch.AStar where
 
 import Control.Monad.State hiding (fail)
-import Data.Map hiding (empty, elem, map, insert)
-import Data.Set hiding (fromList, minView, map)
+import Data.Map hiding (empty, elem, map, insert, filter)
+import Data.Set hiding (fromList, minView, map, size, union, filter)
+
+import Debug.Trace
 
 import GraphSearch
-
-type Heuristic s c = s -> c
-
-data AStarState s o c = AStarState { visited :: Set s, frontier :: Map (c, s) (Path o c) }
-
-data Path o c = Path { pathOps :: [o], pathCost :: c }
-
-astarStep
-  :: (Ord s, SearchProblem s o c, MonadState (AStarState s o c) m)
-  => Heuristic s c -> m (Maybe (Maybe [o]))
-astarStep h = do
-  AStarState vis fron <- get
-  case minViewWithKey fron of
-    Nothing -> pure $ Just Nothing -- no paths found to terminal nodes
-    Just (((c, s), p@(Path ops pc)), fron')
-      | elem s vis -> state $ const (Nothing, AStarState vis fron')
-      | terminal s -> pure $ Just $ Just $ reverse ops
-      | otherwise -> state $ const (Nothing, AStarState (insert s vis) fron'')
-        where
-          costToCome = pc
-          nextOps = legalMoves s
-          nextCosts = cost s <$> nextOps
-          s's = map (flip move s) nextOps
-          hs = map (uncurry (+)) $ zip nextCosts $ map (+ costToCome) $ map h s's
-          fron'' = fromList $ zip (zip hs s's) $ uncurry Path <$> zip (map (: ops) nextOps) (map (+ pc) nextCosts)
-
-runUntilJust :: State s (Maybe a) -> s -> a
-runUntilJust m = go where
-  go s = case runState m s of
-    (Just a, _) -> a
-    (_, s') -> go s'
-
-
-astar :: (Ord s, SearchProblem s o c) => Heuristic s c -> [s] -> Maybe [o]
-astar h ss = runUntilJust (astarStep h) $ AStarState empty frontierInit
-  where
-    frontierInit = fromList $ zip (zip (h <$> ss) ss) $ repeat (Path [] 0)
-
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,13 @@
 module Lib where
 
 import GraphSearch
-import GraphSearch.AStar
+
+import Control.Monad.State
+import Data.Map hiding (empty, elem, map, insert, filter, drop, take, null)
+import Data.Set hiding (fromList, minView, map, size, union, filter, drop, take, null)
+
+import Debug.Trace
+
 
 type Card = Int
 
@@ -15,9 +21,11 @@ data End = Bottom | Top deriving (Eq, Ord, Show)
 
 data Operation = PlaceCard End Int | Stack Int Int deriving Show
 
+{-# INLINE swap #-}
 swap :: (a, a) -> (a, a)
 swap (a1, a2) = (a2, a1)
 
+{-# INLINE choose2 #-}
 choose2 :: Int -> [(Int, Int)]
 choose2 = \case
     n | n < 2 -> []
@@ -25,6 +33,8 @@ choose2 = \case
   where
     sub1 x = x - 1
 
+{-# INLINE isSorted #-}
+{-# SPECIALIZE isSorted :: [Int] -> Bool #-}
 isSorted :: (Ord a) => [a] -> Bool
 isSorted [] = True
 isSorted [a] = True
@@ -33,7 +43,8 @@ isSorted (a1 : a2 : as) = a1 < a2 && isSorted (a2 : as)
 instance SearchProblem BoardState Operation Int where
   legalMoves (BoardState d ps) = case d of
       [] -> uncurry Stack <$> stacks
-      _ | not (null stacks) -> uncurry Stack <$> stacks
+      _
+        | not (null stacks) -> uncurry Stack <$> stacks
         | otherwise -> uncurry PlaceCard <$> places
     where
       allstacks = mappend c2 $ swap <$> c2
@@ -46,7 +57,9 @@ instance SearchProblem BoardState Operation Int where
       c2 = choose2 lps
       lps = length ps
 
-  cost (BoardState d ps) _ = length ps
+  cost _ = \case
+    Stack _ _ -> 1
+    _ -> 0
 
   terminal (BoardState d ps) = length d == 0 && length ps == 1 && isSorted (ps !! 0)
 
@@ -68,7 +81,7 @@ instance SearchProblem BoardState Operation Int where
 
 
 cardheuristic :: BoardState -> Int
-cardheuristic (BoardState d ps) = 0 -- length d + length ps - 1
+cardheuristic (BoardState d ps) = length ps - 1 -- length d + length ps - 1
 
 
 optimalCardSort :: [Card] -> Maybe [Operation]
@@ -79,3 +92,46 @@ intermediateStates :: (SearchProblem s o c) => s -> [o] -> [s]
 intermediateStates s  = \case
   [] -> []
   (flip move s -> s') : os -> s' : intermediateStates s' os
+
+-- ASTAR
+type Heuristic s c = s -> c
+
+data AStarState s o c = AStarState { visited :: Set s, frontier :: Map (c, s) (Path o c) }
+
+data Path o c = Path { pathOps :: [o], pathCost :: c }
+
+{-# INLINE astarStep #-}
+{-# SPECIALIZE astarStep :: Heuristic (BoardState) Int -> State (AStarState BoardState Operation Int) (Maybe (Maybe [Operation])) #-}
+astarStep
+  :: (Ord s, SearchProblem s o c, MonadState (AStarState s o c) m)
+  => Heuristic s c -> m (Maybe (Maybe [o]))
+astarStep h = do
+  AStarState vis fron <- get
+  case minViewWithKey fron of
+    Nothing -> pure $ Just Nothing -- no paths found to terminal nodes
+    Just (((c, s), p@(Path ops pc)), fron')
+      | elem s vis -> state $ const (Nothing, AStarState vis fron')
+      | terminal s -> pure $ Just $ Just $ reverse ops
+      | otherwise -> state $ const (Nothing, AStarState (insert s vis) $ trace (show $ size fron'') fron'')
+        where
+          costToCome = pc
+          nextOps = legalMoves s
+          nextCosts = cost s <$> nextOps
+          s's = map (flip move s) nextOps
+          hs = map (uncurry (+)) $ zip nextCosts $ map (+ costToCome) $ map h s's
+          newFron = fromList $ filter (not . flip elem vis . snd . fst) $ zip (zip hs s's) $ uncurry Path <$> zip (map (: ops) nextOps) (map (+ pc) nextCosts)
+          fron'' = union fron' newFron
+
+{-# INLINE runUntilJust #-}
+runUntilJust :: State s (Maybe a) -> s -> a
+runUntilJust m = go where
+  go s = case runState m s of
+    (Just a, _) -> a
+    (_, s') -> go s'
+
+
+astar :: (Ord s, SearchProblem s o c) => Heuristic s c -> [s] -> Maybe [o]
+astar h ss = runUntilJust (astarStep h) $ AStarState empty frontierInit
+  where
+    frontierInit = fromList $ zip (zip (h <$> ss) ss) $ repeat (Path [] 0)
+
